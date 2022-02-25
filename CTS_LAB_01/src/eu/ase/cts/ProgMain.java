4d1493efeedc0b17b737c22b8a9ac1851bdc9dbb
brosteanuionutc <brosteanu.ionutc@gmail.com>
2022-02-25 13:59:19 +0200

    First commit

---
+++ CTS_LAB_01/src/eu/ase/cts/ProgMain.java
@@ -0,0 +1,15 @@
+package eu.ase.cts;
+
+public class ProgMain {
+
+	public static void main(String[] args) {
+		int i=0;
+		int s=0;
+		
+		int v[]= {0,3,-2,1,0,-2};
+		Utils u;
+		s=Utils.functionCount(v, i);
+		System.out.println(s);
+	}
+
+}
